84fb5f33f3c2fe2edf94087c7e9d549d42ceaa0e
alexandre <xanddybr@gmail.com>
2024-10-18 20:12:50 -0300

    first commit project teste using angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandPg01Component } from "./app/landinpage/land-pg01/land-pg01.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandPg01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/app/landinpage/land-pg01/land-pg01.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-land-pg01',
+  standalone: true,
+  imports: [],
+  templateUrl: './land-pg01.component.html',
+  styleUrl: './land-pg01.component.scss'
+})
+export class LandPg01Component {
+
+  titulo:string = "Meu primeiro projeto com angular 17"
+
+}

6e49d64466441a346db7048265e3fb98fc256962
alexandre <xanddybr@gmail.com>
2024-11-21 02:04:16 +0000

    select boxers finished

--- src/app/catch-lead/catch-lead.component.ts
+++ src/app/catch-lead/catch-lead.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-catch-lead',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './catch-lead.component.html',
   styleUrl: './catch-lead.component.scss'
 })
+
 export class CatchLeadComponent {
+
+  indicacao:any = ["Instagram","FaceBook","YouTube","Google","Recomendação de um amigo"]
+
+  estados:any = ["Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Espírito Santo","Goiás","Maranhão","Mato Grosso","Mato Grosso do Sul","Minas Gerais","Pará","Paraíba","Paraná","Pernambuco","Piauí","Rio de Janeiro","Rio Grande do Norte","Rio Grande do Sul","Rondônia","Santa Catarina","São Paulo","Sergipe","Tocantins"]
 
 }

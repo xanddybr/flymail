56dc370cdd93c329cfc7d0dcf5be50d0a95f5d35
alexandre <xanddybr@gmail.com>
2025-01-21 04:48:47 +0000

    commit 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,8 @@ import { LandPg01Component } from './landing-pages/landing-pg01/land-pg01.compon
 export const routes: Routes = [
 
   {path:'landing_page_program_catarseie_2025_1_edition', component: LandPg01Component},
-  {path:'', redirectTo:'landing_page_program_catarseie_2025_1_edition',pathMatch:'full'}
+  {path:'', redirectTo:'/landing_page_program_catarseie_2025_1_edition',pathMatch:'full'},
+  {path:'**', component: LandPg01Component}
   
 
 ];

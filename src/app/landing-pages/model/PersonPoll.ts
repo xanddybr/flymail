3b2630d988a4d80a89a9c2efe29c4d4ad1a2dfcd
xanddybr <xanddybr@gmail.com>
2025-02-05 15:04:45 -0300

    fixed bugs

---
+++ src/app/landing-pages/model/PersonPoll.ts
@@ -0,0 +1,11 @@
+export interface PersonPoll {
+    id:number
+    name:string
+    lastName:string
+    phone:string
+    email:string
+    state:string
+    rangeAge:string
+    whoAchive:string
+    lifePositon:string
+}
